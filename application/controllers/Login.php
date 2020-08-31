11d8d3188f7e0cbd3013109e957058458602628d
jeversonjv <jeversontp@gmail.com>
2020-08-30 22:56:27 -0300

    configurando pagina de login e cadastro

---
+++ application/controllers/Login.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+
+	public function index() {
+        $this->load->view("login");
+    }
+    
+    public function cadastro() {
+        $this->load->view("cadastro");
+    }
+
+    public function realizar_login() {
+
+    }
+
+    public function salvar_cadastro() {
+    
+    }
+}
