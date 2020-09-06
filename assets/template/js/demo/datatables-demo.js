257ac27fc50ee24e6fa142ae239f48465c87c80a
jeversonjv <jeversontp@gmail.com>
2020-09-06 20:11:45 -0300

    configurando datatables em portugues

--- assets/template/js/demo/datatables-demo.js
+++ assets/template/js/demo/datatables-demo.js
@@ -1,4 +1,8 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    "language": {
+      "url": "https://cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese.json"
+    }
+  });
 });
