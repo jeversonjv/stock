55ac59b982c176f0e91e386fad8b61f30f460f27
jeversonjv <jeversontp@gmail.com>
2020-09-08 01:06:21 -0300

    ajuste footer

--- application/views/template/layout.php
+++ application/views/template/layout.php
@@ -214,7 +214,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
+                        <span>Copyright &copy; Stock <?= date("Y") ?> </span>
                     </div>
                 </div>
             </footer>
