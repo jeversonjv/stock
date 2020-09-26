4fc0a7d2792bd2c3fa9fa8e36d99408a3e83b94a
Jeverson <jeversontp@gmail.com>
2020-09-26 00:47:43 -0300

    colocando campos no cadastro das vendas

--- application/views/Vendas/Formulario.php
+++ application/views/Vendas/Formulario.php
@@ -1,138 +1,80 @@
 <div class="card shadow mb-4">
 
-    <?php if($this->session->flashdata("mensagemErro")) { ?>
+    <?php if ($this->session->flashdata("mensagemErro")) {?>
         <div class="col-lg-12 mb-2 mt-2 text-center">
             <div class="card bg-danger text-white shadow">
                 <div class="card-body">
-                    <?= $this->session->flashdata("mensagemErro") ?> 
+                    <?=$this->session->flashdata("mensagemErro")?>
                 </div>
             </div>
         </div>
-    <?php } ?>
-    
+    <?php }?>
+
     <div class="card-header py-3 d-flex justify-content-between align-items-center">
-        <h6 class="align-middle m-0 font-weight-bold text-primary"><?= !empty($cliente_id) ? "Editar" : (isset($somente_visualizar) ? "Visualizar" : "Adicionar Novo") ?> Cliente</h6>
+        <h6 class="align-middle m-0 font-weight-bold text-primary"><?=!empty($venda_id) ? "Editar" : (isset($somente_visualizar) ? "Visualizar" : "Adicionar Nova")?> Venda</h6>
         <div>
             <a class="btn btn-secondary text-white" href="#" onClick="window.history.back()"> Voltar </a>
-            <?php if(!isset($somente_visualizar)) { ?>
+            <?php if (!isset($somente_visualizar)) {?>
                 <a class="btn btn-primary text-white" id="salvar" > Salvar </a>
-            <?php } ?>
+            <?php }?>
         </div>
     </div>
     <div class="card-body">
-        <form id="formulario" method="post" action="/clientes/salvar">
-            <input type="hidden" name="cliente_id" value="<?= !empty($cliente_id) ? $cliente_id : 0 ?>" />
+        <form id="formulario" method="post" action="/vendas/salvar">
+            <input type="hidden" id="venda_id" name="venda_id" value="<?=!empty($venda_id) ? $venda_id : 0?>" />
 
             <h6 class="align-middle m-0 font-weight-bold">Dados Básicos</h6>
             <hr/>
             <div class="row">
                 <div class="col-md-3 col-xs-12">
-                    <div class="form-group">
-                        <label> Nome* </label>
-                        <input type="text" name="nome" id="nome" class="form-control" value="<?= !empty($cliente->nome) ? $cliente->nome : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-12">
-                    <div class="form-group">
-                        <label> E-mail* </label>
-                        <input type="email" name="email" id="email" class="form-control" value="<?= !empty($cliente->email) ? $cliente->email : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-12">
-                    <div class="form-group">
-                        <label> Data de Nascimento* </label>
-                        <input type="date" name="data_nascimento" id="data_nascimento" class="form-control" value="<?= !empty($cliente->data_nascimento) ? $cliente->data_nascimento : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-12">
-                    <div class="form-group">
-                        <label> Sexo* </label>
-                        <select name="sexo" id="sexo" class="form-control" <?= isset($somente_visualizar) ? "disabled" : "" ?>>
-                            <option value="" <?= !empty($cliente->sexo) ? "" : "selected"?>> Selecione uma opção </option>
-                            <option value="M" <?= !empty($cliente->sexo) && $cliente->sexo === "M" ? "selected" : ""?>>Masculino</option>
-                            <option value="F" <?= !empty($cliente->sexo) && $cliente->sexo === "F" ? "selected" : ""?>>Feminino</option>
-                            <option value="N" <?= !empty($cliente->sexo) && $cliente->sexo === "N" ? "selected" : ""?>>Prefiro não declarar</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-3 col-xs-12">
-                    <div class="form-group">
-                        <label> Telefone </label>
-                        <input type="text" name="telefone" id="telefone" class="form-control" value="<?= !empty($cliente->telefone) ? $cliente->telefone : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-12">
-                    <div class="form-group">
-                        <label> Celular* </label>
-                        <input type="text" name="celular" id="celular" class="form-control" value="<?= !empty($cliente->celular) ? $cliente->celular : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-12">
-                    <div class="form-group">
-                        <label> RG* </label>
-                        <input type="text" name="rg" id="rg" class="form-control" value="<?= !empty($cliente->rg) ? $cliente->rg : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-12">
-                    <div class="form-group">
-                        <label> CPF* </label>
-                        <input type="text" name="cpf" id="cpf" class="form-control" value="<?= !empty($cliente->cpf) ? $cliente->cpf : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-            </div>
-
-            <h6 class="align-middle m-0 font-weight-bold">Endereço do Cliente</h6>
-            <hr/>
-            <div class="row">
-                <div class="col-md-2 col-xs-12">
-                    <div class="form-group">
-                        <label> CEP* </label>
-                        <input type="text" name="cep" id="cep" class="form-control" value="<?= !empty($cliente->cep) ? $cliente->cep : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-                <div class="col-md-4 col-xs-12">
-                    <div class="form-group">
-                        <label> Endereço* </label>
-                        <input type="text" name="endereco" id="endereco" class="form-control" value="<?= !empty($cliente->endereco) ? $cliente->endereco : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-                <div class="col-md-2 col-xs-12">
-                    <div class="form-group">
-                        <label> Bairro* </label>
-                        <input type="text" name="bairro" id="bairro" class="form-control" value="<?= !empty($cliente->bairro) ? $cliente->bairro : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-                <div class="col-md-2 col-xs-12">
-                    <div class="form-group">
-                        <label> Número* </label>
-                        <input type="text" name="numero" id="numero" class="form-control" value="<?= !empty($cliente->numero) ? $cliente->numero : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-                <div class="col-md-2 col-xs-12">
-                    <div class="form-group">
-                        <label> Cidade* </label>
-                        <input type="text" name="cidade" id="cidade" class="form-control" value="<?= !empty($cliente->cidade) ? $cliente->cidade : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-12">
-                    <div class="form-group">
-                        <label> Estado* </label>
-                        <select name="estado" id="estado" class="form-control" <?= isset($somente_visualizar) ? "disabled" : "" ?>>
-                            <option value="" <?= !empty($cliente->sexo) ? "" : "selected"?>>Selecione uma opção</option>
-                            <?php foreach(estadosBrasileiros() as $sigla => $estado) { ?>
-                                <option value="<?= $sigla ?>" <?= !empty($cliente->estado) && $cliente->estado === $sigla ? "selected" : ""?>> <?= $estado ?> </option>
-                            <?php } ?>
-                        </select>
-                    </div>
+                    <label> Cliente* </label>
+                    <select name="cliente_id" id="cliente_id" class="form-control form-control-chosen" <?=isset($somente_visualizar) ? "disabled" : ""?>>
+                        <option value="" <?=!empty($venda->cliente_id) ? "" : "selected"?>>Selecione uma opção</option>
+                        <?php foreach($clientes as $cliente) { ?>
+                            <option value="<?=$cliente->cliente_id?>" <?=!empty($venda->cliente_id) && $cliente->cliente_id == $venda->cliente_id ? "selected" : ""?>> <?=$cliente->nome?> </option>
+                        <?php } ?>
+                    </select>
                 </div>
                 <div class="col-md-9 col-xs-12">
                     <div class="form-group">
-                        <label> Complemento </label>
-                        <input type="text" name="complemento" id="complemento" class="form-control" value="<?= !empty($cliente->complemento) ? $cliente->complemento : ""?>" <?= isset($somente_visualizar) ? "readonly" : "" ?> />
+                        <label> Descrição </label>
+                        <input type="text" name="complemento" id="complemento" class="form-control" value="<?=!empty($venda->descricao) ? $venda->descricao : ""?>" <?=isset($somente_visualizar) ? "readonly" : ""?> />
                     </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between align-items-center mt-2">
+                <h6 class="align-middle font-weight-bold">Produtos da Venda</h6>
+                <a class="btn btn-primary text-white" id="salvar"> Adicionar Produto </a>
+            </div>
+
+            <div class="row mt-2">
+                <div class="col-md-12 col-xs-12">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th> Nome </th>
+                                <th> Quantidade </th>
+                                <th> Valor Unitário </th>
+                                <th> Valor Total (Qtd x V.U) </th>
+                                <th> Remover </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> PC Gamer </td> 
+                                <td> 2 </td>
+                                <td> R$ 3.500,00 </td>
+                                <td> R$ 7.000,00 </td>
+                                <td> <a class="btn btn-danger text-white btn-deletar"> <i class="fas fa-trash"></i></i> </a> </td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="1"> <strong> Total </strong> </td>
+                                <td colspan="4"> <strong> R$ 7.000,00 </strong> </td>
+                            </tr>
+                        </tfoot>
+                    </table>
                 </div>
             </div>
         </form>
